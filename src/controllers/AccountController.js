d299b061013986f74638d7e4ed6a5613e817858c
Marcus Lima <marcus_fonseca@ymail.com>
2021-02-22 00:05:32 -0300

    query only active user accounts

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -24,6 +24,9 @@ module.exports = {
 
       if(userId) {
         const accounts = await Account.findAll({
+          where: {
+            active: true
+          },
           include: {
             association: 'team',
             include: {
